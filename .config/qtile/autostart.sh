f4109f3182b2bfb2efe1f5bd872acba1f35a3068
niz <156370431+nizaralheet@users.noreply.github.com>
2024-10-16 13:45:58 +0300

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,6 +2,8 @@
 
 picom --blur-method dual_kawase --blur-strength 5 --vsync --crop-shadow-to-monitor --animations &
 wal -R &&
+udiskie &
 nohup dunst &
 sleep &
+./.config/qtile/idle-manager.sh &
 
